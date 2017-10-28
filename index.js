a707feae40308242d65698bd9a331d4bb877bdb4
James Conkling <james.lane.conkling@gmail.com>
2017-10-28 15:16:21 -0400

    update module exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.get = require('./lib/get');
+module.exports.request = require('./lib/get');
 module.exports.entry2JSON = require('./lib/parse').entry2JSON;
 module.exports.json2Triple = require('./lib/parse').json2Triple;
 module.exports.linter = require('./lib/linter');
