fe0516cb682b5760534e99b7776b771e7e8c733f
James Conkling <james.lane.conkling@gmail.com>
2017-10-28 13:59:12 -0400

    export module bindings for get and linter

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+module.exports.get = require('./lib/get');
 module.exports.entry2JSON = require('./lib/parse').entry2JSON;
 module.exports.json2Triple = require('./lib/parse').json2Triple;
+module.exports.linter = require('./lib/linter');
