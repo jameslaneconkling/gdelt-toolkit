911e5af01ffdfbcd8a0461ea090a1b2e8a03d2e9
James Conkling <james.lane.conkling@gmail.com>
2017-10-30 17:48:02 -0400

    bugfix paths to cache

--- src/clean.js
+++ src/clean.js
@@ -1,17 +1,17 @@
 const {
   readdir,
-  makedirSync,
+  mkdirSync,
   unlinkSync
 } = require('fs');
 
 module.exports = () => {
-  readdir(`${__dirname}/cache/`, (err, files) => {
+  readdir(`${__dirname}/../cache/`, (err, files) => {
     if (err && err.code === 'ENOENT') {
-      return makedirSync(`${__dirname}/cache/`);
+      return mkdirSync(`${__dirname}/../cache/`);
     } else if (err) {
       return console.error(err);
     }
 
-    files.forEach((file) => file !== '.gitkeep' && unlinkSync(`${__dirname}/cache/${file}`));
+    files.forEach((file) => file !== '.gitkeep' && unlinkSync(`${__dirname}/../cache/${file}`));
   });
 };
